dc28c44bfc9dd2140306136dcf8b6d91e055b580
Arthur Lucas <arthur.lucas.melo@gmail.com>
2026-06-25 16:31:39 -0300

    doc: adiciona o banner inicial do jogo

---
+++ adivinhe.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    //Imprime a introdução do jogo
+    printf("##########################\n");
+    printf("#        Adivinhe!       #\n");
+    printf("##########################\n");
+    return 0;
+}
