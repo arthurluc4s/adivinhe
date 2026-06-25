cd25a38464d128d5eb4d8465a05e472f3e2d11c7
Arthur Lucas <arthur.lucas.melo@gmail.com>
2026-06-25 16:44:20 -0300

    feat: implementa a leitura do chute do usuário

--- adivinhe.c
+++ adivinhe.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
 int main() {
+    //Variáveis
+    int numerosecreto = 42;
+    int chute;
+
     //Imprime a introdução do jogo
     printf("##########################\n");
     printf("#        Adivinhe!       #\n");
     printf("##########################\n");
+
+    //Lê chute do usuário
+    printf("Digite seu chute: ");
+    scanf("%d", &chute);
+
+    printf("Você chutou o número: %d\n", chute);
+
     return 0;
 }
